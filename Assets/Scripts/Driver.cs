a6797a1612717e96fe5164d7ea9caf5d7c938b46
Luke McRae <lukemcrae2006@gmail.com>
2025-09-10 11:02:41 -0400

    Video04_ifStatement

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
-    float steerSpeed = 0.5f;
-    float moveSpeed = 01f;
+    [SerializeField] float steerSpeed = 0.5f;
+    [SerializeField] float moveSpeed = 01f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,24 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            Debug.Log("We are pushing forward");
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            Debug.Log("We are pushing downard");
+        }
+
+        if (Keyboard.current.aKey.isPressed)
+        {
+            Debug.Log("We are pushing left");
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            Debug.Log("We are pushing right");
+        }
         transform.Rotate(0, 0, steerSpeed);
         transform.Translate(0, moveSpeed, 0);
     }
