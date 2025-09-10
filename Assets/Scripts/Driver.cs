18e4386753b25e6f0c9a6d34d144eb4882481f69
Luke McRae <lukemcrae2006@gmail.com>
2025-09-10 13:42:56 -0400

    Last Update, Added  more acceleration

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,25 +4,33 @@ using UnityEngine.InputSystem;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 0.5f;
-    [SerializeField] float moveSpeed = 01f;
+    [SerializeField] float moveSpeed = 0f;
+    float maxSpeed = 80f;
+    float minSpeed = 10f;
+    float lastMovDir = 1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        moveSpeed = minSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         float steer = 0f;
         float move = 0f;
         if (Keyboard.current.wKey.isPressed)
         {
             move = 1f;
+            lastMovDir = 1f;
         }
         else if (Keyboard.current.sKey.isPressed)
         {
             move = -1f;
+            lastMovDir = -1f;
         }
 
         if (Keyboard.current.aKey.isPressed)
@@ -34,9 +42,41 @@ public class Driver : MonoBehaviour
             steer = -1f;
         }
 
-        float moveAmount = move * moveSpeed * Time.deltaTime;
-        float steerAmount = steer * steerSpeed * Time.deltaTime;
+
+        if (move != 0)
+        {
+            if (moveSpeed < maxSpeed)
+            {
+                moveSpeed += Time.deltaTime * 15f;
+            }
+
+        }
+        else
+        {
+            if (moveSpeed > minSpeed)
+            {
+                moveSpeed -= Time.deltaTime * 30f;
+            }
+        }
+        float moveAmount;
+
+
+        if (moveSpeed > minSpeed && move == 0)
+        {
+
+            moveAmount = moveSpeed * lastMovDir * Time.deltaTime;
+        }
+        else
+        {
+            moveAmount = move * moveSpeed * Time.deltaTime;
+        }
+
+        float steerAmount = steer * steerSpeed * moveSpeed * Time.deltaTime;
         transform.Translate(0, moveAmount, 0);
-        transform.Rotate(0, 0, steerAmount);
+        if (moveSpeed > minSpeed)
+        {
+            transform.Rotate(0, 0, steerAmount);
+        }
+
     }
 }
