5c27ba23aee42e7337b1c1195fc8cbdd9f04a7d7
Luke McRae <lukemcrae2006@gmail.com>
2025-09-17 13:40:07 -0400

    Video07_Destroy Object
    Destroys obect, collects package, delivers package

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage;
+    [SerializeField] private float delay = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Package") && !hasPackage)
+        {
+            Debug.Log("Picked up a package");
+            hasPackage = true;
+            Destroy(collision.gameObject, delay);
+        }
+
+        if (collision.CompareTag("Customer") && hasPackage)
+        {
+            Debug.Log("Delivered Package");
+            hasPackage = false;
+        }
+
+    }
+}
